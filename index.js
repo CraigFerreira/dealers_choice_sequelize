ef91c5ee2bd24153cd2d2f91e02f79efd6f9724f
Craig Ferreira <ferreira.craig@gmail.com>
2021-02-21 14:11:40 -0500

    added project to git

---
+++ index.js
@@ -0,0 +1,59 @@
+const {syncAndSeed, People, Tasks}= require('./server.js')
+
+const express= require('express');
+const app= express();
+const port = process.env.PORT || 3000;
+
+const setup=async()=>{
+    try{
+        app.listen(port, ()=>{
+            console.log(`listening on port ${port}`)
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+setup()
+
+app.get('/', async(request, response, next)=>{
+    try{
+        const people= await People.findAll()
+        const tasks= await Tasks.findAll();
+        response.send(`
+        <html>
+            <head></head>
+            <body>
+                <h1>Dealers Choice Sequelize</h1>
+                <ul>
+                    ${people.map((person)=>{
+                        return `<li>${person.name}
+                            <ul>
+                                    ${tasks.map((task)=>{
+                                        if(task.PersonId===person.id){
+                                            let currTask=task.name;
+                                            let currTaskId=task.id
+                                            let currTaskDependancy=task.dependencyId;
+                                            return `<li>
+                                            ${task.name}
+                                                <ul>
+                                                    ${tasks.map((task)=>{ 
+                                                        if(task.id===currTaskDependancy)
+                                                        return  `<li style='list-style-type: none'>Dependant on : ${task.name}</li>`
+                                                    }).join('')}
+                                                </ul>
+                                            </li>`  
+                                        }
+                                    }).join('')}  
+                            </ul>
+                        </li>`
+                    }).join('')}
+                <ul>
+            </body>
+        </html>
+        `)
+    }catch(err){
+        next(err)
+    }
+})
---
+++ server.js
@@ -0,0 +1,61 @@
+const {Sequelize, DataTypes, UUID, UUIDV4} = require('sequelize')
+const db =new Sequelize('postgres://localhost/tasks_db', {logging: false})
+
+const test=async()=>{
+    try{
+    await db.authenticate();
+    console.log('database connected')
+    } catch(err){
+        console.error('connection failed',err)
+    }
+}
+
+test()
+
+const People= db.define('People',{
+    name:{type: DataTypes.STRING, allowNull: false},
+    id:{type: UUID, primaryKey: true, defaultValue: UUIDV4},
+    occupation:{type: DataTypes.STRING, allowNull: false}
+})
+
+const Tasks=db.define('Tasks',{
+    name:{type: DataTypes.STRING, allowNull: false},
+    id:{type: UUID, primaryKey: true, defaultValue: UUIDV4},
+    dependencies:{type: DataTypes.STRING, allowNull:true, defaultValue: 'none'}
+})
+
+Tasks.belongsTo(People);
+Tasks.belongsTo(Tasks, {as: 'dependency'})
+
+
+const syncAndSeed=async()=>{
+    await People.sync({force: true})
+    await Tasks.sync({force:true})
+    const harry=await People.create({name: 'Harry', occupation:'retailer'})
+    const john= await People.create({name:'John',occupation:'banker' })
+    const andrew= await People.create({name: 'Andrew', occupation:'accountant'})
+
+    const bakeCake= await Tasks.create({name:'bake cake', dependencies: 'buy cake ingredients'})
+    const buyCakeIngredients= await Tasks.create({name: 'buy cake ingredients'})
+    const walkDog= await Tasks.create({name: 'walk the Dog'})
+    const completeWorkProject= await Tasks.create({name:'complete work project'})
+    const vacation= await Tasks.create({name:'go on vacation', dependencies: 'complete work project'})
+
+    bakeCake.PersonId= harry.id;
+    buyCakeIngredients.PersonId=harry.id;
+    walkDog.PersonId=john.id;
+    completeWorkProject.PersonId=andrew.id;
+    vacation.PersonId= andrew.id;
+
+    bakeCake.dependencyId= buyCakeIngredients.id;
+    vacation.dependencyId= completeWorkProject.id;
+    await Promise.all([bakeCake.save(), buyCakeIngredients.save(), walkDog.save(), bakeCake.save(), completeWorkProject.save(), vacation.save()])
+}
+
+
+syncAndSeed();
+
+
+module.exports={
+    syncAndSeed, People, Tasks
+}
