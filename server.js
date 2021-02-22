3c587bf4dd7004bf4b0b89da66cffb41880ddf91
Craig Ferreira <ferreira.craig@gmail.com>
2021-02-22 13:43:54 -0500

    made changes

--- server.js
+++ server.js
@@ -31,37 +31,42 @@ Tasks.belongsTo(Tasks, {as: 'dependency'})
 
 
 const syncAndSeed=async()=>{
-    await People.sync({force: true})
-    await Tasks.sync({force:true})
-    const harry=await People.create({name: 'Harry', occupation:'retailer'})
-    const john= await People.create({name:'John',occupation:'banker' })
-    const andrew= await People.create({name: 'Andrew', occupation:'accountant'})
+    try{
+        await People.sync({force: true})
+        await Tasks.sync({force:true})
+        const harry=await People.create({name: 'Harry', occupation:'retailer'})
+        const john= await People.create({name:'John',occupation:'banker' })
+        const andrew= await People.create({name: 'Andrew', occupation:'accountant'})
+    
+        const bakeCake= await Tasks.create({name:'bake cake', dependencies: 'buy cake ingredients'})
+        const buyCakeIngredients= await Tasks.create({name: 'buy cake ingredients'})
+        const walkDog= await Tasks.create({name: 'walk the Dog'})
+        const completeWorkProject= await Tasks.create({name:'complete work project'})
+        const vacation= await Tasks.create({name:'go on vacation', dependencies: 'complete work project'})
+        const attendGraduation= await Tasks.create({name:'attend graduation', dependencies:'pass exam'})
+        const passExam =await Tasks.create({name: 'pass the exam', dependencies:'study for exam'})
+        const studyForExam= await Tasks.create({name: 'study for exam'})
+    
+        bakeCake.PersonId= harry.id;
+        buyCakeIngredients.PersonId=harry.id;
+        walkDog.PersonId=john.id;
+        completeWorkProject.PersonId=andrew.id;
+        vacation.PersonId= andrew.id;
+        attendGraduation.PersonId=andrew.id;
+        passExam.PersonId=andrew.id;
+        studyForExam.PersonId=andrew.id;
+    
+        bakeCake.dependencyId= buyCakeIngredients.id;
+        vacation.dependencyId= completeWorkProject.id;
+        attendGraduation.dependencyId= passExam.id;
+        passExam.dependencyId=studyForExam.id
+        await Promise.all([bakeCake.save(), buyCakeIngredients.save(), walkDog.save(), completeWorkProject.save(), vacation.save(),
+            attendGraduation.save(), passExam.save(), studyForExam.save(), 
+        ]) 
+    } catch(err){
+        console.log(err)
+    }
 
-    const bakeCake= await Tasks.create({name:'bake cake', dependencies: 'buy cake ingredients'})
-    const buyCakeIngredients= await Tasks.create({name: 'buy cake ingredients'})
-    const walkDog= await Tasks.create({name: 'walk the Dog'})
-    const completeWorkProject= await Tasks.create({name:'complete work project'})
-    const vacation= await Tasks.create({name:'go on vacation', dependencies: 'complete work project'})
-    const attendGraduation= await Tasks.create({name:'attend graduation', dependencies:'pass exam'})
-    const passExam =await Tasks.create({name: 'pass the exam', dependencies:'study for exam'})
-    const studyForExam= await Tasks.create({name: 'study for exam'})
-
-    bakeCake.PersonId= harry.id;
-    buyCakeIngredients.PersonId=harry.id;
-    walkDog.PersonId=john.id;
-    completeWorkProject.PersonId=andrew.id;
-    vacation.PersonId= andrew.id;
-    attendGraduation.PersonId=andrew.id;
-    passExam.PersonId=andrew.id;
-    studyForExam.PersonId=andrew.id;
-
-    bakeCake.dependencyId= buyCakeIngredients.id;
-    vacation.dependencyId= completeWorkProject.id;
-    attendGraduation.dependencyId= passExam.id;
-    passExam.dependencyId=studyForExam.id
-    await Promise.all([bakeCake.save(), buyCakeIngredients.save(), walkDog.save(), completeWorkProject.save(), vacation.save(),
-        attendGraduation.save(), passExam.save(), studyForExam.save(), 
-    ])
 }
 
 
